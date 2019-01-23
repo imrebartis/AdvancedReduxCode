4db8d0cd4eadc9b94eb25f2ff03ac0851c8c2d9c
Imre Bartis <imre.bartis@siili.com>
2019-01-23 07:54:42 +0200

    verify component existence

--- testing/src/components/__tests__/App.test.js
+++ testing/src/components/__tests__/App.test.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom';
 import App from '../App';
 
 it('shows a comment box', () => {
-    const div = document.createAttribute('div');
+    const div = document.createElement('div');
 
     ReactDOM.render(<App/>, div);
 
     // placeholder for code that looks inside the div
     // & checks to see if the CommentBox is in there
+    expect(div.innerHTML).toContain('Comment Box');
 
     ReactDOM.unmountComponentAtNode(div);
 });
