d559ceccb79c3452388f37567179ce8937d88c3b
Imre Bartis <imre.bartis@siili.com>
2019-02-09 19:35:09 +0200

    implement textarea functionality

--- testing/src/components/CommentBox.js
+++ testing/src/components/CommentBox.js
@@ -1,11 +1,26 @@
 import React, { Component } from "react";
 
 class CommentBox extends Component {
+  state = { comment: "" };
+
+  handleChange = event => {
+    this.setState({ comment: event.target.value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    // TODO call an action creator
+    // save the comment
+
+    this.setState({ comment: "" });
+  };
+
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <h4>Add a comment</h4>
-        <textarea />
+        <textarea onChange={this.handleChange} value={this.state.comment} />
         <div>
           <button>Submit coment</button>
         </div>
