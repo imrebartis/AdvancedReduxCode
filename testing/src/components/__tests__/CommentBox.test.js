d6bb6164eb63cbb7f58abc5795bd53552ccb58cf
Imre Bartis <imre.bartis@siili.com>
2019-02-13 19:08:39 +0200

    use afterEach

--- testing/src/components/__tests__/CommentBox.test.js
+++ testing/src/components/__tests__/CommentBox.test.js
@@ -2,9 +2,19 @@ import React from "react";
 import { mount } from "enzyme";
 import CommentBox from "components/CommentBox";
 
-it('has a text area and a button', () => {
-  const wrapped = mount(<CommentBox />);
+let wrapped;
 
+beforeEach(() => {
+  wrapped = mount(< CommentBox />);
+});
+
+afterEach(() => {
+  // do cleanup, since full rendering mounts the component in the DOM & thus tests
+  // can accidentally affect each other if they are using the same DOM
+  wrapped.unmount();
+})
+
+it('has a text area and a button', () => {
   expect(wrapped.find('textarea').length).toEqual(1);
   expect(wrapped.find('button').length).toEqual(1);
 })
