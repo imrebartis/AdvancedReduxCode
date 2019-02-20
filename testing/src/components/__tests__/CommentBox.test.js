18b1cf18b68530760c368c55b0006926749bf5fb
Imre Bartis <imre.bartis@siili.com>
2019-02-20 19:40:06 +0200

    use a describe statement to add a beforeEach that applies only to textarea related tests

--- testing/src/components/__tests__/CommentBox.test.js
+++ testing/src/components/__tests__/CommentBox.test.js
@@ -5,38 +5,36 @@ import CommentBox from "components/CommentBox";
 let wrapped;
 
 beforeEach(() => {
-  wrapped = mount(< CommentBox />);
+  wrapped = mount(<CommentBox />);
 });
 
 afterEach(() => {
   // do cleanup, since full rendering mounts the component in the DOM & thus tests
   // can accidentally affect each other if they are using the same DOM
   wrapped.unmount();
-})
+});
 
-it('has a text area and a button', () => {
-  expect(wrapped.find('textarea').length).toEqual(1);
-  expect(wrapped.find('button').length).toEqual(1);
-})
+it("has a text area and a button", () => {
+  expect(wrapped.find("textarea").length).toEqual(1);
+  expect(wrapped.find("button").length).toEqual(1);
+});
 
-it('has a text area that users can type in', () => {
-  wrapped.find('textarea').simulate('change', {
-    target: { value: 'new comment'}
+describe("the text area", () => {
+  beforeEach(() => {
+    wrapped.find("textarea").simulate("change", {
+      target: { value: "new comment" }
+    });
+    // force the component to update as soon as the change has been simulated:
+    wrapped.update();
   });
-  // force the component to update as soon as the change has been simulated:
-  wrapped.update();
 
-  expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
-})
-
-it('when form is submitted, the text area gets emptied', () => {
-  wrapped.find('textarea').simulate('change', {
-    target: { value: 'new comment'}
+  it("has a text area that users can type in", () => {
+    expect(wrapped.find("textarea").prop("value")).toEqual("new comment");
   });
-  // force the component to update as soon as the change has been simulated:
-  wrapped.update();
-  
-  wrapped.find('form').simulate('submit');
-  wrapped.update();
-  expect(wrapped.find('textarea').prop('value')).toEqual('');
-})
+
+  it("when form is submitted, the text area gets emptied", () => {
+    wrapped.find("form").simulate("submit");
+    wrapped.update();
+    expect(wrapped.find("textarea").prop("value")).toEqual("");
+  });
+});
