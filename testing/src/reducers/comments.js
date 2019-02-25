7994404343766e594bff6b33656a13206b0ed1b8
Imre Bartis <imre.bartis@siili.com>
2019-02-25 19:19:12 +0200

    add SaveComment action creator

--- testing/src/reducers/comments.js
+++ testing/src/reducers/comments.js
@@ -1,5 +1,10 @@
+import { SAVE_COMMENT } from 'actions/types';
+
 export default function(state = [], action) {
   switch (action.type) {
+    case SAVE_COMMENT:
+      //take all the existing comments 6 add them to a new array
+      return [...state, action.payload];
     default: return state;
   }
 }
