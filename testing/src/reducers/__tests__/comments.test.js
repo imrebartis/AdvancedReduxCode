6591fd2335d99388413bfc1b4547d5dfd0314fdc
Imre Bartis <imre.bartis@siili.com>
2019-03-03 09:03:42 +0200

    test reducers

---
+++ testing/src/reducers/__tests__/comments.test.js
@@ -0,0 +1,13 @@
+import commentsReducers from "reducers/comments";
+import { SAVE_COMMENT } from "actions/types";
+
+it("handles actions of type SAVE_COMMENT", () => {
+  const action = {
+    type: SAVE_COMMENT,
+    payload: "new comment"
+  };
+
+  const newState = commentsReducers([], action);
+
+  expect(newState).toEqual(['new comment']);
+});
