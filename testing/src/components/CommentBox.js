ac19ce3e2da848bfe9ad3be69f95befa62689831
Imre Bartis <imre.bartis@siili.com>
2019-03-11 11:22:54 +0200

    parse comment list

--- testing/src/components/CommentBox.js
+++ testing/src/components/CommentBox.js
@@ -17,15 +17,21 @@ class CommentBox extends Component {
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <h4>Add a comment</h4>
-        <textarea onChange={this.handleChange} value={this.state.comment} />
-        <div>
-          <button>Submit coment</button>
-        </div>
-      </form>
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <h4>Add a comment</h4>
+          <textarea onChange={this.handleChange} value={this.state.comment} />
+          <div>
+            <button>Submit coment</button>
+          </div>
+        </form>
+        <button onClick={this.props.fetchComments}>Fetch comments</button>
+      </div>
     );
   }
 }
 
-export default connect(null, actions)(CommentBox);
+export default connect(
+  null,
+  actions
+)(CommentBox);
--- testing/src/reducers/comments.js
+++ testing/src/reducers/comments.js
@@ -1,10 +1,14 @@
-import { SAVE_COMMENT } from 'actions/types';
+import { SAVE_COMMENT, FETCH_COMMENTS } from 'actions/types';
 
 export default function(state = [], action) {
   switch (action.type) {
     case SAVE_COMMENT:
-      //take all the existing comments 6 add them to a new array
+      //take all the existing comments & add them to a new array
       return [...state, action.payload];
+    case FETCH_COMMENTS:
+      const comments = action.payload.data.map(comment => comment.name);
+      //take all the existing comments & the newly added comments & add them to a new array
+      return [...state, ...comments]
     default: return state;
   }
 }
