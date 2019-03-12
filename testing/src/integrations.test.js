49af3e4ac9ba8f0423b0223c6094c4dfb8f9ef7e
Imre Bartis <imre.bartis@siili.com>
2019-03-12 11:37:05 +0200

    start work on integration testing

---
+++ testing/src/integrations.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import Root from 'Root';
+import App from 'components/App';
+
+it('can fetch a list of comments & display them', () => {
+    // attempt to render the entire app
+    // find the fetchComments button & click it
+    // expect to find a list of comments
+})
