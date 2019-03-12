3bb6a27b2b1426c587c716091aae1ad179d645ef
Imre Bartis <imre.bartis@siili.com>
2019-03-12 11:51:35 +0200

    render the entire app in the integration test; fix a broken unit test

--- testing/src/integrations.test.js
+++ testing/src/integrations.test.js
@@ -4,7 +4,13 @@ import Root from 'Root';
 import App from 'components/App';
 
 it('can fetch a list of comments & display them', () => {
-    // attempt to render the entire app
+    // render the entire app
+    const wrapped = mount(
+        // add the Provider tag (which here goes under the name of Root) to App so that the entire app could be tested
+        <Root>
+          <App />
+        </Root>
+        );
     // find the fetchComments button & click it
     // expect to find a list of comments
 })
