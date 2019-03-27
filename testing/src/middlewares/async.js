e75e22d3be4344a8ff83750ced42410b5f4590c8
Imre Bartis <imre.bartis@siili.com>
2019-03-27 13:27:23 +0200

    if no promise, forward async action to next middleware

--- testing/src/middlewares/async.js
+++ testing/src/middlewares/async.js
@@ -1,1 +1,9 @@
-export default ({ dispatch }) => next => (action) => {};
+export default ({ dispatch }) => next => action => {
+  // check to see if the action
+  // has a promise on its 'payload' property
+  //if it does, wait for it to resolve
+  //if it doesn't, send the action on to the next middleware
+  if (!action.payload || !action.payload.then) {
+    return next(action);
+  }
+};
