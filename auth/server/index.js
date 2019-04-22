847a274791d83199d1b3530131077ebd89a4ac18
Imre Bartis <imre.bartis@siili.com>
2019-04-22 05:24:44 +0300

    configure the database & connect to it

--- auth/server/index.js
+++ auth/server/index.js
@@ -1,17 +1,28 @@
-const express = require('express');
-const http = require('http');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
+const express = require("express");
+const http = require("http");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
 const app = express();
-const router = require('./router');
+const router = require("./router");
+const mongoose = require("mongoose");
+
+// DB Config
+
+const db = "mongodb://localhost:auth/auth";
+
+// Connect DB
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("MongoDB Connected..."))
+  .catch(err => console.log(err));
 
 // App setup
-app.use(morgan('combined'));
-app.use(bodyParser.json({ type: '*/*' }));
+app.use(morgan("combined"));
+app.use(bodyParser.json({ type: "*/*" }));
 router(app);
 
 // Server setup
 const port = process.env.PORT || 3090;
 const server = http.createServer(app);
 server.listen(port);
-console.log('Server listening on', port);
+console.log("Server listening on", port);
