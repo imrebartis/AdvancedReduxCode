65627525b352cdb44d8a14ea0eeac4ac982ff542
Imre Bartis <imre.bartis@siili.com>
2019-04-27 05:29:37 +0300

    set up search for users

--- auth/server/controllers/authentication.js
+++ auth/server/controllers/authentication.js
@@ -1,3 +1,17 @@
+const User = require("../models/user");
+
 exports.signup = function(req, res, next) {
-  res.send({ success: "true" });
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // see if a user with a given email exists
+  User.findOne({ email : email }, function(err, existingUser) {
+
+  })
+
+  // if user with email does exist, return an error
+
+  // if user with email does not exist, create & save user record
+
+  // respond to request indicating the user was created
 }
