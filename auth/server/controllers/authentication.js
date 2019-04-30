6312428c6a016c246f6f67de3405ebcd002d634e
Imre Bartis <imre.bartis@siili.com>
2019-04-30 07:09:36 +0300

    create user records

--- auth/server/controllers/authentication.js
+++ auth/server/controllers/authentication.js
@@ -6,12 +6,24 @@ exports.signup = function(req, res, next) {
 
   // see if a user with a given email exists
   User.findOne({ email : email }, function(err, existingUser) {
+    if (err) { return next(err) }
 
+    // if user with email does exist, return an error
+    if (existingUser) {
+      return res.status(422).send({ error: 'Email is in use'} );
+    }
   })
 
-  // if user with email does exist, return an error
-
   // if user with email does not exist, create & save user record
+  const user = new User({
+    email,
+    password
+  })
 
-  // respond to request indicating the user was created
+  user.save(function(err) {
+    if (err) { return next(err) }
+
+    // respond to request indicating the user was created
+    res.json( {success: true} );
+  });
 }
