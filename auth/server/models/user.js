cd683e1bccedb4987ab07e2098f33b5d570f5b79
Imre Bartis <imre.bartis@siili.com>
2019-05-05 09:25:12 +0300

    salt password

--- auth/server/models/user.js
+++ auth/server/models/user.js
@@ -9,19 +9,24 @@ const userSchema = new Schema({
 });
 
 // on save hook encrypt password
+// before saving model run this function
 userSchema.pre("save", function(next) {
+  // get access to the user model
   const user = this;
 
+  // generate a salt then run callback
   bcrypt.genSalt(10, function(err, salt) {
     if (err) {
       return next(err);
     }
 
+    // hash (=encrypt) password using the salt
     bcrypt.hash(user.password, salt, null, function(err, hash) {
       if (err) {
         return next(err);
       }
 
+      // overwrite plain text password with encrypted password
       user.password = hash;
       next();
     });
